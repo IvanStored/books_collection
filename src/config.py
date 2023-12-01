cb73997503e51a779139ce2070f10437d2d0e5a6
IvanStored <jus1stored@gmail.com>
2023-12-01 23:36:42 +0200

    add base for tests

--- src/config.py
+++ src/config.py
@@ -9,11 +9,16 @@ class DataBaseSettings(BaseSettings):
     DB_HOST: str
     DB_PORT: str
     DB_NAME: str
+    DB_TEST_NAME: str
+    DB_TEST_PORT: str
 
     model_config = SettingsConfigDict(env_file=find_dotenv(".env"))
 
     def get_db_url(self) -> str:
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    def get_test_db_url(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_TEST_PORT}/{self.DB_TEST_NAME}"
 
 
 settings = DataBaseSettings()
